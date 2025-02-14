6fdcdb0af246c8d9fc6fbb2eabc19f5e98943160
carlos uchoa <carlosjuchoa@hotmail.com>
2025-02-14 12:48:48 -0300

    enviando pedido.service.ts

--- frontend/src/pedidos/pedidos.service.ts
+++ frontend/src/pedidos/pedidos.service.ts
@@ -1,51 +1,25 @@
-import { Injectable } from "@nestjs/common"
-import { InjectRepository } from "@nestjs/typeorm"
-import type { Repository } from "typeorm"
-import { Pedido } from "./pedido.entity"
-import { PedidoProduto } from "./pedido-produto.entity"
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Pedido } from './pedido.entity';
 
 @Injectable()
-export class PedidosService {
+export class PedidoService {
   constructor(
     @InjectRepository(Pedido)
-    private pedidosRepository: Repository<Pedido>,
-    @InjectRepository(PedidoProduto)
-    private pedidoProdutosRepository: Repository<PedidoProduto>,
+    private pedidoRepository: Repository<Pedido>,
   ) {}
 
-  findAll(): Promise<Pedido[]> {
-    return this.pedidosRepository.find({ relations: ["cliente", "pedidoProdutos", "pedidoProdutos.produto"] })
+  async findAll(): Promise<Pedido[]> {
+    return this.pedidoRepository.find();
   }
 
-  findOne(id: number): Promise<Pedido> {
-    return this.pedidosRepository.findOne({
-      where: { id },
-      relations: ["cliente", "pedidoProdutos", "pedidoProdutos.produto"],
-    })
+  async create(pedidoData: Pedido): Promise<Pedido> {
+    const novoPedido = this.pedidoRepository.create(pedidoData);
+    return this.pedidoRepository.save(novoPedido);
   }
 
-  async create(pedido: Pedido): Promise<Pedido> {
-    const novoPedido = await this.pedidosRepository.save(pedido)
-    for (const pedidoProduto of pedido.pedidoProdutos) {
-      pedidoProduto.pedidoId = novoPedido.id
-      await this.pedidoProdutosRepository.save(pedidoProduto)
-    }
-    return this.findOne(novoPedido.id)
-  }
-
-  async update(id: number, pedido: Pedido): Promise<Pedido> {
-    await this.pedidosRepository.update(id, { clienteId: pedido.clienteId, total: pedido.total })
-    await this.pedidoProdutosRepository.delete({ pedidoId: id })
-    for (const pedidoProduto of pedido.pedidoProdutos) {
-      pedidoProduto.pedidoId = id
-      await this.pedidoProdutosRepository.save(pedidoProduto)
-    }
-    return this.findOne(id)
-  }
-
-  async remove(id: number): Promise<void> {
-    await this.pedidoProdutosRepository.delete({ pedidoId: id })
-    await this.pedidosRepository.delete(id)
+  async delete(id: number): Promise<void> {
+    await this.pedidoRepository.delete(id);
   }
 }
-
